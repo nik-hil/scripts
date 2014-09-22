0dc01c68e133e169fee2ae5e133b758054756572
nik-hil <nikhil.ikhar@gmail.com>
2014-09-22 11:09:18 +0530

    Removed unnecessary flags

--- palindrome.py
+++ palindrome.py
@@ -3,75 +3,49 @@ center of pallindrome. From center it probes in both direction this pallindrome
 exists.
 A pallindrome might exists in space between two characters e.g, "bb"
 '''
-
-def pallindrome(string):
-    print ">>>>>>>> string: " + string
+def palindrome(string):
+    if not string:
+        return None
+    max_range = ()
+    max_length = 0
     for i in range(len(string)):
-        max_range = find_pallindrome(i, string)
-        print "max_range: "+ str(max_range)
-        if max_range[1] == max_range[0]:
-            print "pallindrome: " + string[max_range[0]]   
-        else:
-            print "pallindrome: " + string[max_range[0]:max_range[1]+1]
-
-def find_pallindrome(i, string):
-    j = 0
-    low = i-j
-    high = i+j
-    flag1 = False # if while loop has not started. pallindrome is that char
-    flag2 = False
-    try :
-        while string[low] == string[high]:
-            flag1 = True
-            j += 1
-            low = i-j
-            high = i+j
-            if low < 0: # -ve index points towards end.
-                break
-            
-    except Exception, e:
-        print "first: ", str(e)
-    finally:
-        j -=1
-        if flag1:
-            pal_len1 = i+j -(i-j) + 1
-            pal_range1 = (i-j, i+j)
-        else:
-            pal_len1 = 0
-            pal_range1 = (i,i)
-
-    try :
-        j = 0
-        low = i-j
-        high = i+j +1
-        while string[low] == string[high]:
-            flag2 = True
-            j += 1
-            low = i-j
-            high = i+j +1
-            if low < 0:
-                break
-    except Exception, e:
-        print "Second: ", str(e)
+        current_range = find_pallindrome(i, string)
+        new_length = current_range[1] - current_range[0] + 1
+        if max_length < new_length :
+            max_length = new_length
+            max_range = current_range
+    return string[max_range[0]:max_range[1] + 1]
         
-    finally:
-        j -=1
-        if flag2:
-            pal_len2 = i+1+j -(i-j) + 1
-            pal_range2 = (i-j, i+1+j)
-        else:
-            pal_len2 = 0
-            pal_range2 = (i,i)
-
-
-    if pal_len1 > pal_len2:
-        return pal_range1
+def find_pallindrome(i, string):
+    len_str = len(string)
+    len_first = 0
+    len_second = 0
+    low = i
+    high = i
+    while (low > -1 and high < len_str and string[low] == string[high]):
+        low -= 1
+        high += 1
+    if low == i and high == i:
+        len_first = (low, high)
     else:
-        return pal_range2
+        len_first = (low + 1, high - 1)
+    low = i
+    high = i + 1
+    while(low > -1 and high < len_str and string[low] == string[high]):
+        low -= 1
+        high += 1
+    if low == i and high == i + 1:
+        len_second = (low, high - 1)
+    else:
+        len_second = (low + 1, high - 1)
+    if len_first[1] - len_first[0] > len_second[1] - len_second[0]:
+        return len_first
+    else:
+        return len_second
 
-
-pallindrome("a")
-pallindrome("ab")
-pallindrome("bb")
-pallindrome("abcba")
-pallindrome("efabcba")
+print palindrome("")
+print palindrome("a")
+print palindrome("ab")
+print palindrome("bb")
+print palindrome("abcba")
+print palindrome("efabcba")
